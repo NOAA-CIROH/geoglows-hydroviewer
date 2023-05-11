499ba70b89eee05315dc35bb5394c840b6a2797a
romer8 <gio.busrom@gmail.com>
2023-05-11 15:44:51 +0000

    tag version v1.9_0_2023_05_11

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
-app_package = 'geoglows_hydroviewer'
-release_package = 'tethysapp-' + app_package
+namespace = 'tethysapp'
+app_package = "geoglows_hydroviewer"
+release_package = "tethysapp-" + app_package
 
 # -- Python Dependencies -- #
 dependencies = []
 
 # -- Get Resource File -- #
-resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+resource_files = find_all_resource_files(app_package, namespace)
+
 
 setup(
     name=release_package,
-    version='1.9',
-    description='GUI for the GEOGloWS ECMWF Streamflow model',
-    long_description='Contains an interactive map for viewing modeled streamflow using the GEOGloWS data and mapping '
-                     'web services. Includes tools for extracting a subset of the delineated shapefiles and creating '
-                     'a customized hydroviewer interface.',
-    keywords='geoglows, ecmwf, streamflow',
-    author='Riley Hales',
-    author_email='',
-    url='https://geoglows.ecmwf.int',
-    license='BSD 3-Clause Clear',
+    version="1.9",
+    description="GUI for the GEOGloWS ECMWF Streamflow model",
+    long_description="GUI for the GEOGloWS ECMWF Streamflow model",
+    keywords="replace_keywords",
+    author="Riley Hales",
+    author_email="Riley Hales_email",
+    url="https://geoglows.ecmwf.int",
+    license="BSD 3-Clause Clear",
     packages=find_namespace_packages(),
-    package_data={'': resource_files},
+    package_data={"": resource_files},
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
